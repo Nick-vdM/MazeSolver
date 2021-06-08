d35256cd326e359a966cce25ae41ecbe2b669ba5
Nick-vdM <nick.jvdmerwe@gmail.com>
2021-06-08 18:09:33 +1000

    Functional except moving one at a time is funky
    Took 1 hour 12 minutes

--- maze_generator/maze_generator.cpp
+++ maze_generator/maze_generator.cpp
@@ -1,7 +1,5 @@
-//
 // Created by nicol on 7/06/2021.
-// https://www.linkedin.com/in/nick-vd-merwe/
-// nick.jvdmerwe@gmail.com
+// nick.vandermerwe@griffithuni.edu.au
 
 // Implements a recursive backtracking algorithm to write
 // a maze into the mazes directory
@@ -19,31 +17,6 @@
 #include <cstdio>
 #include <random>
 
-/**
- * This is actually ripped out of the boost library, but I didn't want to go
- * through the pain of installing boost just so I can make a single hash
- * function, and it means that people running this don't have to either.
- *
- * Note, however, that these recommend stdex::hash_value and I'm using
- * std::hash. From what I saw, stdex::hash is just the VS C++ version (?)
- *
- * See https://stackoverflow.com/questions/16471691/good-hash-function-for-pair-of-primitive-types
- * @return
- */
-template<typename T>
-void hash_combine(size_t &seed, T const &v) {
-    seed ^= std::hash<T>(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
-
-template<class T>
-struct pairHash {
-    std::size_t operator()(const std::pair<T, T> p) const {
-        size_t firstHash = std::hash<T>(p.first);
-        hash_combine(firstHash, p.second);
-        return firstHash;
-    }
-};
-
 
 std::string currentDateTime() {
     time_t now = time(nullptr);
@@ -56,22 +29,11 @@ std::string currentDateTime() {
     return buffer;
 }
 
-struct cellInformation {
-    cellInformation(std::pair<uint32_t, uint32_t> location,
-                    std::vector<std::pair<uint32_t, uint32_t>> freeAdjacents) :
-            location{std::move(location)},
-            freeAdjacents{std::move(freeAdjacents)} {}
-
-    std::pair<uint32_t, uint32_t> location;
-    std::vector<std::pair<uint32_t, uint32_t>> freeAdjacents;
-};
-
 class RecursiveBacktrackingMaze {
 public:
     RecursiveBacktrackingMaze(uint32_t &x, uint32_t &y, uint32_t seed) :
             x(x), y(y), rng(std::default_random_engine(seed)),
-            random0to3(std::uniform_int_distribution<uint8_t>(0, 3)),
-            random0touint32_t(std::uniform_int_distribution<uint32_t>(
+            randomInt(std::uniform_int_distribution<uint32_t>(
                     0, std::numeric_limits<uint32_t>::max())) {
         /**
          * Fills the maze vector in storage with a set seed
@@ -90,21 +52,21 @@ public:
         // In a backtracking algorithm like this, an odd number is always
         // an open wall.
         this->startPos = std::pair<uint32_t, uint32_t>{
-                ((this->random0touint32_t(this->rng) % x) / 2) * 2,
-                ((this->random0touint32_t(this->rng) % y) / 2) * 2
+                ((this->randomInt(this->rng) % x) / 2) * 2,
+                ((this->randomInt(this->rng) % y) / 2) * 2
         };
 
         this->goalPos = std::pair<uint32_t, uint32_t>{
-                ((this->random0touint32_t(this->rng) % x) / 2) * 2,
-                ((this->random0touint32_t(this->rng) % y) / 2) * 2
+                ((this->randomInt(this->rng) % x) / 2) * 2,
+                ((this->randomInt(this->rng) % y) / 2) * 2
         };
 
         while (startPos == goalPos) {
             // In case it made the exact same number, keep trying to make
             // more until they're different
             this->goalPos = std::pair<uint32_t, uint32_t>{
-                    ((this->random0touint32_t(this->rng) % x) / 2) * 2,
-                    ((this->random0touint32_t(this->rng) % y) / 2) * 2
+                    ((this->randomInt(this->rng) % x) / 2) * 2,
+                    ((this->randomInt(this->rng) % y) / 2) * 2
             };
         }
 
@@ -129,6 +91,15 @@ public:
 
     std::vector<std::vector<char>> getMaze() {
         return maze;
+    }
+
+    void printMaze(){
+        for(auto & line : this->maze){
+            for(auto & cell : line){
+                std::cout << cell;
+            }
+            std::cout << std::endl;
+        }
     }
 
     bool saveMaze(const std::string &path) {
@@ -168,94 +139,71 @@ private:
     std::vector<std::vector<char>> maze;
     /// We can look up a random direction with this
     std::vector<std::pair<int8_t, int8_t>> moves{
-            {0,  2},
-            {0,  -2},
-            {2,  0},
-            {-2, 0}
+            {0,  1},
+            {0,  -1},
+            {1,  0},
+            {-1, 0}
     };
 
     std::default_random_engine rng;
-    std::uniform_int_distribution<uint8_t> random0to3;
-    std::uniform_int_distribution<uint32_t> random0touint32_t;
+    std::uniform_int_distribution<uint32_t> randomInt;
 
     void generateMaze() {
         /**
          * Sets the this->maze vector based on the current format
          */
-        std::stack<cellInformation> pathTaken;
-        std::unordered_set<std::pair<uint32_t, uint32_t>, pairHash<uint32_t>>
-                visitedCells;
-        pathTaken.emplace(startPos,
-                          listSolidAdjacentCells(startPos));
-        uint32_t pops = 0;
+        std::stack<std::pair<uint32_t, uint32_t>> pathTaken;
+        pathTaken.emplace(startPos);
+        // Start pos should already be marked so no need to worry
+        // about marking it
+
         while (!pathTaken.empty()) {
             auto &currentCell = pathTaken.top();
-            // Roll back until currentCell has adjacent ones
-            while (currentCell.freeAdjacents.empty()) {
+            auto adjacent = listSolidAdjacentCells(currentCell);
+            while (adjacent.empty()) {
                 pathTaken.pop();
+                if(pathTaken.empty()) break;
                 currentCell = pathTaken.top();
+                adjacent = listSolidAdjacentCells(currentCell);
             }
-            // Pick a random free one
-            uint8_t toMoveTo = random0to3(rng) %
-                               currentCell.freeAdjacents.size();
 
-            // Insert it into the stack as it is the next move
+            if(pathTaken.empty() && adjacent.empty()) break;
+
+            uint8_t &&randomChoice = randomInt(rng) % adjacent.size();
             pathTaken.emplace(
-                    currentCell.freeAdjacents[toMoveTo],
-                    listSolidAdjacentCells(currentCell.freeAdjacents[toMoveTo])
+                    (2 * adjacent[randomChoice].first) + currentCell.first,
+                    (2 * adjacent[randomChoice].second) + currentCell.second
             );
 
-            // Pop it out of the last one
-            currentCell.freeAdjacents.erase(
-                    currentCell.freeAdjacents.begin() + toMoveTo
-            );
-
-            // Update the maze
-            markMazeClear(currentCell.location,
-                          pathTaken.top().location);
+            MoveTwoAndClear(currentCell, adjacent[randomChoice]);
         }
     }
 
-    std::vector<std::pair<uint32_t, uint32_t>> listSolidAdjacentCells(
+    std::vector<std::pair<int8_t, int8_t>> listSolidAdjacentCells(
             std::pair<uint32_t, uint32_t> coord) {
-        std::vector<std::pair<uint32_t, uint32_t>> clearCells;
+
+        std::vector<std::pair<int8_t, int8_t>> solidCells;
         for (auto &m : moves) {
-            // TODO: Validate that this can subtract
             auto adjacent = std::pair<uint32_t, uint32_t>{
                     m.first + coord.first,
                     m.second + coord.second
             };
             if (isSolid(adjacent)) {
-                clearCells.push_back(adjacent);
+                solidCells.emplace_back(m);
             }
         }
-        return clearCells;
+        return solidCells;
     }
 
-    void
-    markMazeClear(std::pair<uint32_t, uint32_t> &coord1, std::pair<uint32_t,
-            uint32_t> &coord2) {
-        /// Marks all the cells between coord1 and coord2 as clear if they are
-        /// walls
-
-        // First find the direction that they are different in
-        bool firstAxis = false;
-        if (coord1.first != coord2.first) {
-            firstAxis = true;
-        }
-
-        // TODO: Verify these coord1/second operations are correct
-        if (firstAxis) {
-            for (uint32_t i = coord1.first; i == coord2.first; i++) {
-                if (this->maze[i][coord1.second] == 'H') {
-                    this->maze[i][coord1.second] = 'O';
-                }
-            }
-        } else {
-            for (uint32_t i = coord1.second; i == coord2.second; i++) {
-                if (this->maze[coord1.first][i] == 'H') {
-                    this->maze[coord1.first][i] = 'O';
-                }
+    void MoveTwoAndClear(
+            std::pair<uint32_t, uint32_t> coord,
+            std::pair<int8_t, int8_t> &movement) {
+        /// Marks two cells in front/behind of coord
+        for (int i = 0; i < 2; i++) {
+            coord.first += movement.first;
+            coord.second += movement.second;
+            if (isSolid(coord)) {
+                this->maze[coord.first][coord.second] = 'O';
             }
         }
     }
@@ -288,8 +236,11 @@ int main(int argc, char **argv) {
     uint32_t x = strtol(argv[1], nullptr, 10);
     uint32_t y = strtol(argv[2], nullptr, 10);
 
+    std::cout << "Starting..." << std::endl;
     auto maze = RecursiveBacktrackingMaze(x, y, 42);
     maze.saveMaze();
+    maze.printMaze();
+    std::cout << std::endl << "Done" << std::endl;
 
     return EXIT_SUCCESS;
 }
